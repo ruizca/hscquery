c6c350b21e345e6855a754c7da39f0dbec565666
Ruiz Angel <angel.ruizca@gmail.com>
2018-10-13 18:17:29 +0300

    Fix documentation

--- docs/conf.py
+++ docs/conf.py
@@ -17,15 +17,15 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 
-from mock import Mock as MagicMock
- 
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
- 
-MOCK_MODULES = ['astropy']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+#from mock import Mock as MagicMock
+# 
+#class Mock(MagicMock):
+#    @classmethod
+#    def __getattr__(cls, name):
+#        return MagicMock()
+# 
+#MOCK_MODULES = ['astropy']
+#sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
 
